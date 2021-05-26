74dd4446906d10f274f80bb83b49cb3518255209
Minh Ho <hothieuminh@gmail.com>
2021-05-27 00:10:41 +0700

    draw new case plot

--- app.R
+++ app.R
@@ -10,10 +10,15 @@ ui <- fluidPage(
   selectInput(inputId = "destination", label = "Destination", choices = WHO_data$Name),
   numericInput(inputId = "LengthOfStay", label = "Length of stay", value =  10, min = 1, max = 180),
   selectInput(inputId = "Vacinnation", label = "Vaccinated?", choices = c("Yes", "No")),
-  selectInput(inputId = "Condition", label = "Which condition(s) do you have?", choices = Consonance$List.of.diseases))
+  selectInput(inputId = "Condition", label = "Which condition(s) do you have?", choices = Consonance$List.of.diseases),
+  mainPanel(
+    textOutput("summary")
+  ))
 
 server <- function(input, output) {
-
+  output$summary <- renderText({ 
+    paste("You are going to", input$destination, "for", input$LengthOfStay, "days")
+  })
 }
 shinyApp(ui = ui, server = server)
 
